dc2d35149bc8ebe8f28b84ade77f3e44dcdd730a
user <user@es301n26>
2019-03-05 12:03:54 +0800

    list and set

--- myproject/src/com/tom/Phone.java
+++ myproject/src/com/tom/Phone.java
@@ -10,10 +10,10 @@ public class Phone {
 		call.add("您撥的電話通話中,請稍後再撥");
 		call.add("您撥的電話通話中,請稍後再撥");
 		call.add("您撥的電話通話中,請稍後再撥");
-		call.add("您撥的電化已關機,請稍後再撥");
-		call.add("您撥的電化已關機,請稍後再撥");
-		call.add("您撥的電化已關機,請稍後再撥");
-		call.add("您撥的電化已關機,請稍後再撥");
+		call.add("您撥的電已關機,請稍後再撥");
+		call.add("您撥的電話已關機,請稍後再撥");
+		call.add("您撥的電話已關機,請稍後再撥");
+		call.add("您撥的電話已關機,請稍後再撥");
 		System.out.println(call);
 
 	}
